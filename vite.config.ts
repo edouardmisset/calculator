0343d465c37ad84913fc64c66f2707df27d6ba10
ext_em <edouard.misset@intest.info>
2023-09-07 15:26:20 +0200

    fix(deploy): fix deploy url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calculator',
+  // base: '/calculator',
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
-    globals: true
-  }
+    globals: true,
+  },
 })
